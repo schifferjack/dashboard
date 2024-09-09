4f47ad808c89d45384c0b387ff52ebc47234d8c4
Syariz Ashraf <s.hisham@1stdigital.com>
2024-09-09 18:56:18 +0800

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SignUpComponent },
+  { path: 'home', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+];
